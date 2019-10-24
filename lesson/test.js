5ac359a27c3c6656ee9a25252226a562ac266ead
Santiago Quintero Hincapié <santiago.quinteroh@udea.edu.co>
2019-10-24 16:01:09 -0500

    Lesson_17

--- lesson/test.js
+++ lesson/test.js
@@ -1,5 +1,14 @@
 // alert("Hello World!")
 
-var myVar1 = 10;
-var myVar2 = "hi";
-alert(myVar1 + myVar2);
+// var myVar1 = 10;
+// var myVar2 = "hi";
+// alert(myVar1 + myVar2);
+
+var links = document.getElementsByTagName("a");
+
+for (i = 0; i < links.length; i++) {
+    console.log(links[i].outerHTML);
+    console.log(links[i].innerText);
+}
+
+console.log(links)
